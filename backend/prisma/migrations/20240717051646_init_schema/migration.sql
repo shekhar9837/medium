48f5d7c622108e22faaceb8c542b84f1e24181dc
Chandra Shekhar <mauryashekhar9837@gmail.com>
2024-07-17 23:03:42 +0530

    added routes

--- backend/prisma/migrations/20240717051646_init_schema/migration.sql
+++ backend/prisma/migrations/20240717051646_init_schema/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" TEXT,
     "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
